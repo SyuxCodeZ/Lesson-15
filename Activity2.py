1fc0e8d2238b60b9c932cec361b3624b4506e37a
SyuxCodeZ <cuberdytm@gmail.com>
2025-02-05 17:51:17 +0600

    rf;oiywqrflkujghawerf;kuihgqwerf;uihqwer.kujgqwerg

--- Activity2.py
+++ Activity2.py
@@ -1,7 +1,11 @@
-x = 69
-y = 420
-
-if x < y:
-    pass
-else:
-    print ("y>x")
+for x in range(30):
+    if (x % 15) == 0:
+        pass
+    elif (x % 20) == 0:
+        print ("fizz")
+    elif (x % 5) == 0:
+        print ("buzz")
+    elif (x % 3) == 0:
+        print ("yes")
+    else:
+        print (x)
